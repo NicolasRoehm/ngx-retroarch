9da84b29c9c6288c776edaf7129bcaece9110e47
NicolasRoehm <nicolas.roehm@gmail.com>
2021-07-02 22:10:48 +0200

    [Upd] HUD & readme + [Fix] build

--- README.md
+++ README.md
@@ -2,13 +2,15 @@
 
 RetroArch as a web component.
 
-**TODO**
+## Roadmap
 - Update player key binding
-- Update volume
-- Full screen
-- Save and load game state
-- Save screenshot
-- Update usage chapter
+- Save & load game state
+- Save & download saved screenshot
+- Auto detect core
+- Add cores + check if there is the same "queueAudio" function in all unminified cores
+- Pause game / Speed / Slow
+- Update usage chapter (readme)
+- Fix retroarch quality
 
 ## Usage
 
@@ -18,13 +20,34 @@ The project isn't stable enough. It will be available on NPM as a web component.
 <ngx-retroarch core="snes9x" rom="Goof_Troop.zip"></ngx-retroarch>
 ```
 
+The following attributes can be set on ngx-retroarch:
+- **core** - Choose one the following:
+  `genesis_plus_gx`, `mgba`, `mupen64plus_next`, `nestopia`, `snes9x`
+
+- **rom** - The rom file name with its extension. For example: `Goof_Troop.zip`.
+- **rom-path** - The path to the rom (default is `./`). You have to define the path to the rom without the rom file. For example: `https://www.example.com/assets/snes/`
+- **core-path** - The path to the core (default is `./`). Cores are included into the NPM package so you don't have to update the path unless you want to import them by yourself.
+
+### NPM
+
+```sh
+// Not available yet
+npm install @bakudan/ngx-retroarch
+```
+
+### CDN
+
+
+
 ## Demo
 
-1. Build and serve the project
+1. Add your rom(s) in the `src/assets/roms` directory
+2. Edit the `rom` and `core` attributes in `src/assets/demo.html`
+3. Build and serve the project
 ```sh
 npm run demo
 ```
-2. Navigate to `http://127.0.0.1:4300/`.
+4. Navigate to http://127.0.0.1:4300/.
 
 
 ## Development
@@ -33,23 +56,27 @@ npm run demo
 ```sh
 git clone https://github.com/NicolasRoehm/ngx-retroarch
 cd ./ngx-retroarch
-npm i
+npm install
 ```
-2. Build the library in watch mode
+
+2. Add your rom(s) in the `src/assets/roms` directory
+3. Edit the `rom` and `core` attributes in `src/app/app.component.html`
+
+4. Build the library in watch mode
 ```sh
 npm run watch:all
 ```
-3. Start the dev server in a new terminal
+5. Once the library is watched, start the dev server in a new terminal
 ```sh
 npm run start
 ```
-4. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+6. Navigate to http://localhost:4200/. The app will automatically reload if you change any of the source files.
 
 ### Architecture
 
 | File | Purpose |
 | --- | --- |
-| [emulator.component.ts](https://github.com/NicolasRoehm/ngx-retroarch/blob/master/projects/components/src/lib/components/emulator/emulator.component.ts) | This contains the main logic of the package. |
+| [emulator.component.ts](https://github.com/NicolasRoehm/ngx-retroarch/blob/master/projects/components/src/lib/components/emulator/emulator.component.ts) [emulator.component.html](https://github.com/NicolasRoehm/ngx-retroarch/blob/master/projects/components/src/lib/components/emulator/emulator.component.html) | This contains the main logic of the package. |
 | [app.component.html](https://github.com/NicolasRoehm/ngx-retroarch/blob/master/src/app/app.component.html) | This contains the development code which uses the package as an Angular component. |
 | [demo.html](https://github.com/NicolasRoehm/ngx-retroarch/blob/master/src/assets/demo.html) | This contains the final code which uses the package as a web component *(once built)*. This file is `cp` to the `dist/elements` folder during the build process. |
 
--- projects/components/src/lib/components/hud/hud.component.ts
+++ projects/components/src/lib/components/hud/hud.component.ts
@@ -1,8 +1,9 @@
 // Angular modules
-import { Component, ElementRef } from '@angular/core';
-import { OnInit }    from '@angular/core';
+import { Component }  from '@angular/core';
+import { ElementRef } from '@angular/core';
+import { OnInit }     from '@angular/core';
 
-declare const Browser : any;
+declare const RA : any;
 
 @Component({
   selector    : 'ngx-hud',
@@ -11,6 +12,12 @@ declare const Browser : any;
 })
 export class HudComponent implements OnInit
 {
+  public  isFullscreen  : boolean = false;
+  public  isPaused      : boolean = false;
+  public  defaultVolume : number  = 100;
+  public  volume        : number  = this.defaultVolume;
+
+  private audioOverride : boolean = false;
 
   constructor
   (
@@ -26,28 +33,86 @@ export class HudComponent implements OnInit
 
   public async ngOnInit() : Promise<void>
   {
-
+    // (RA.context as AudioContext).onstatechange = (ev) => {
+    //   console.log(ev);
+    //   console.log(RA.context.state);
+    // };
   }
 
   // -------------------------------------------------------------------------------
   // ---- NOTE Actions -------------------------------------------------------------
   // -------------------------------------------------------------------------------
 
-  public onClickFullscreen() : void
+  public onChangeVolume(volume : number) : void
   {
-    // this.elementRef
-    // console.log(this.elementRef.nativeElement.parentElement);
-    // let el = document.getElementById('#retroarch-container');
-    // console.log(el);
-    // if (!el)
-    //   return;
-    this.elementRef.nativeElement.parentElement.requestFullscreen();
-    // Browser.requestFullscreen(true, false);
+    // FIXME Gain can be controlled in runtime with input_volume_up/input_volume_down/
+
+    this.volume = volume * 0.01;
+
+    // NOTE Overide audio loop
+    if (!this.audioOverride)
+    {
+      this.overrideQueueAudio();
+      this.audioOverride = true;
+      return;
+    }
+  }
+
+  public onClickTogglePlayPause() : void
+  {
+    if (!this.isPaused)
+    {
+      this.isPaused = true;
+      window['Module'].pauseMainLoop();
+      return;
+    }
+    window['Module'].resumeMainLoop();
+    this.isPaused = false;
+  }
+
+  public async onClickToggleFullscreen() : Promise<void>
+  {
+    if (!this.isFullscreen)
+    {
+      (this.elementRef.nativeElement.parentElement as Element).requestFullscreen();
+      this.isFullscreen = true;
+      return;
+    }
+    // TODO Listen on "escape" pressed (exited fullscreen)
+    document.exitFullscreen();
+    this.isFullscreen = false;
   }
 
   // -------------------------------------------------------------------------------
   // ---- NOTE Helpers -------------------------------------------------------------
   // -------------------------------------------------------------------------------
+
+  private overrideQueueAudio() : void
+  {
+    // It comes from unminified snes9x_libretro.js file at line 1266
+    RA.queueAudio = function () {
+      var index = RA.bufIndex;
+      var startTime;
+      if (RA.bufIndex) startTime = RA.buffers[RA.bufIndex - 1].endTime;
+      else startTime = RA.context.currentTime;
+      RA.buffers[index].endTime = startTime + RA.buffers[index].duration;
+      const bufferSource = RA.context.createBufferSource();
+      bufferSource.buffer = RA.buffers[index];
+      // bufferSource.connect(RA.context.destination); // NOTE Commented
+      this.setVolume(bufferSource); // NOTE Added
+      bufferSource.start(startTime);
+      RA.bufIndex++;
+      RA.bufOffset = 0;
+    }.bind(this);
+  }
+
+  private setVolume(bufferSource : AudioBufferSourceNode) : void
+  {
+    const gainNode = RA.context.createGain();
+    gainNode.connect(RA.context.destination);
+    bufferSource.connect(gainNode);
+    gainNode.gain.setValueAtTime(this.volume, RA.context.currentTime);
+  }
 
   // -------------------------------------------------------------------------------
   // ---- NOTE Subscription --------------------------------------------------------
